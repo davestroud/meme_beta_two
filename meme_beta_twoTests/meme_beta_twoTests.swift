253c241b6abeae91aafca43f8766a55006c52dde
David Stroud <david@greymatter-ag.com>
2015-09-08 17:49:24 -0500

    first commit

---
+++ meme_beta_twoTests/meme_beta_twoTests.swift
@@ -0,0 +1,36 @@
+//
+//  meme_beta_twoTests.swift
+//  meme_beta_twoTests
+//
+//  Created by John David Stroud on 9/8/15.
+//  Copyright © 2015 John David Stroud. All rights reserved.
+//
+
+import XCTest
+@testable import meme_beta_two
+
+class meme_beta_twoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
